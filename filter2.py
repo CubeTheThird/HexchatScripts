627a22efcb375284b516ca7b58de497dada4d5b6
CubeTheThird <cubethethird@gmail.com>
2016-06-26 20:34:00 -0400

    Experimental changes to filter script.

--- filter2.py
+++ filter2.py
@@ -1,8 +1,9 @@
 __module_name__ = 'Filter2'
-__module_version__ = '3.7'
+__module_version__ = '4.0-beta'
 __module_description__ = 'Filters join/part messages by hosts'
 
 import hexchat
+#Import based on python version
 import sys
 if sys.version_info[0] < 3:
 	import urllib2
@@ -13,12 +14,10 @@ from time import time
 import socket
 
 last_seen = {}	# List where key is host
-				# 0 : last seen
+				# 0 : last seen time
 				# 1 : boolean has spoken (0 false 1 true)
 				# 2 : previous username
 user_timeout = 600	#ignore if timeout is surpassed
-
-halt = False
 
 debug_output = False
 
@@ -39,68 +38,69 @@ def human_readable(s):
 
 def new_msg(word, word_eol, event, attrs):
 	#handles normal messages
-	global halt
 	global last_seen
-	if halt == True:
-		return
 
-	user = hexchat.strip(word[0])
+	#get username from message
+	user_name = hexchat.strip(word[0])
+
 	users = hexchat.get_list("users")
-	host = "NULL"
+
+	#Try to find the user based on the users list
+	user = "NULL"
+	user_ip = "NULL"
 	for u in users:
-		if(u.nick == user):
-			host = u.host
+		if(u.nick == user_name):
+			user = u.host
+			user_ip = get_ip(user)
+			break
 
 	#user was logged in before script started
-	if host not in last_seen:
-		last_seen[host] = [time(), 1, user]
+	if user_ip not in last_seen:
+		last_seen[user_ip] = [time(), 1, user_name]
 
-	#invalid host case
-	if host == "" or host == "NULL":
-		halt = True
+	#invalid user case (typically user is no longer connected)
+	if  user_ip == "NULL" or user == "NULL":
 		hexchat.emit_print(event, *word)
-		halt = False
 		if debug_output:
-			print("\00315Supressed invalid host name")
+			print("\00315Supressed invalid user case")
 		return hexchat.EAT_ALL
 
 	#user never spoke before
-	if last_seen[host][1] == 0:
-		time_diff = time() - last_seen[host][0]
-
-		ip = get_ip(host)
+	if last_seen[user_ip][1] == 0:
+		time_diff = time() - last_seen[user_ip][0]
 
 		#get geoip
-		geoip = get_geoip(host)
+		geoip = get_geoip(user_ip)
 
-		if user == last_seen[host][2]:
-			word[1] += " \00307(logged in %s ago from \00302%s \00310%s\00307)" % (human_readable(time_diff),host.split('@')[0] + '@' + ip,geoip)
+		#add information to message
+		if user_name == last_seen[user_ip][2]:
+			word[1] += " \00307(logged in %s ago from \00302%s \00310%s\00307)" % (human_readable(time_diff),user.split('@')[0] + '@' + ip,geoip)
 		else:
-			word[1] += " \00307(logged in %s ago. Formerly \00302%s\00307 from \00302%s \00310%s\00307)" % (human_readable(time_diff),last_seen[host][2],host.split('@')[0] + '@' + ip,geoip) #added host for debug purposes
-			last_seen[host][2] = user
-		halt = True
+			word[1] += " \00307(logged in %s ago. Formerly \00302%s\00307 from \00302%s \00310%s\00307)" % (human_readable(time_diff),last_seen[user_ip][2],user.split('@')[0] + '@' + ip,geoip) #added host for debug purposes
+			last_seen[user_ip][2] = user_name
+		#print message
 		hexchat.emit_print(event, *word)
-		halt = False
-		last_seen[host][1] = 1
-		return hexchat.EAT_ALL
+		last_seen[user_ip][1] = 1
+		return hexchat.PLUGIN
 	else:
-		last_seen[host][0] = time()
+		last_seen[user_ip][0] = time()
 
 def filter_msg(word, word_eol, event, attrs):
 
-	global halt
 	global last_seen
-	if halt == True:
-		return
 
 	#filter join and part messages
-	user = hexchat.strip(word[0])
-	host = "NULL"
+	user_name = hexchat.strip(word[0])
+	user = "NULL"
+	user_ip = "NULL"
+
 	#Join event
 	if event == "Join":
-		host = hexchat.strip(word[2])
-		ip = get_ip(host)
-		if debug_output:
+		user = hexchat.strip(word[2])
+		user_ip = get_ip(user)
+
+		#TODO: correct this to display changes in user id
+		if False:
 			host2 = "NULL"
 			for u in hexchat.get_list("users"):
 				if(u.nick == word[1]):
@@ -108,73 +108,79 @@ def filter_msg(word, word_eol, event, attrs):
 					if host != host2:
 						print("\00315---Inconsistent host error between " + host + " and " + host2 + " ---")
 					break
-		if host not in last_seen:
-			last_seen[host] = [time(), 0, user]
+
+		if user_ip not in last_seen:
+			#First time join from IP
+			last_seen[user_ip] = [time(), 0, user_name]
 			if debug_output:
-				print("\00315Supressed join of " + user + " from " + host)
+				print("\00315Supressed join of " + user_name + " from " + user_ip)
 			return hexchat.EAT_ALL
-		elif(last_seen[host][2] != user):
-			if last_seen[host][1] == 1:
-				word[2] = "Formerly \00302%s\00307 from \00302%s \00310%s\00307" % (last_seen[host][2],ip,get_geoip(host))
-				halt = True
+		elif(last_seen[user_ip][2] != user_name):
+			#User logged in with different name
+			if last_seen[user_ip][1] == 1:
+				word[2] = "Formerly \00302%s\00307 from \00302%s \00310%s\00307" % (last_seen[user_ip][2],user_ip,get_geoip(user_ip))
 				hexchat.emit_print(event, *word)
-				halt = False
-			last_seen[host][2] = user
+			last_seen[user_ip][2] = user_name
 			return hexchat.EAT_ALL
-		elif last_seen[host][0] + user_timeout > time():
-			last_seen[host][0] = time()
+		#Update user time
+		last_seen[user_ip][0] = time()
+
 	#Change username event
 	elif event == "Change Nick":
-		for idx, h in enumerate(last_seen):
-			if(last_seen[h][2] == user):
-				host = h
+
+		#Find user in seen list
+		for idx, ip in enumerate(last_seen):
+			if(last_seen[ip][2] == user_name):
+				user_ip = ip
 				break
-		if(host == "NULL"):
+		#User was not in seen list
+		if(user_ip == "NULL"):
+			#Find user in users list
 			for u in hexchat.get_list("users"):
 				if(u.nick == word[1]):
-					host = u.host
-					last_seen[host] = [time(), 0, user]
+					user = u.host
+					user_ip = get_ip(user)
+					last_seen[user_ip] = [time(), 0, user_name]
 					break
-		if host == "NULL" and debug_output:
-			print("\00315Error in Change Nick event: NULL host")
-		last_seen[host][2] = word[1]
-		if last_seen[host][0] + user_timeout > time():
-			last_seen[host][0] = time()
-	#find host
-	for idx, h in enumerate(last_seen):
-		if(last_seen[h][2] == user):
-			host = h
-			break
+		#User was not in either list
+		if user_ip == "NULL" and debug_output:
+			print("\00315Error in Change Nick event: NULL ip")
 
-	#if user joined before chat started, quit or something before talking
-	if(host == "NULL"):
+		#Continue as though nothing is wrong :Z
+
+		#Update username and time
+		last_seen[user_ip][2] = word[1]
+		last_seen[user_ip][0] = time()
+
+	#User parted or quit before sending a message
+	if(user_ip == "NULL"):
 		if debug_output:
-			print("\00315Supressed NULL host output for " + user + " from event " + event)
+			print("\00315Supressed NULL host output for " + user_name + " from event " + event)
 		return hexchat.EAT_ALL
 
 	#if user never spoke, or spoke too long ago
-	if last_seen[host][1] == 0:
+	if last_seen[user_ip][1] == 0:
 		if debug_output:
-			print("\00315Supressed new user event " + event + " for user " + user)
+			print("\00315Supressed new user event " + event + " for user " + user_name)
 		return hexchat.EAT_ALL
-	if ( last_seen[host][0] + user_timeout < time() and event not in ["Join", "Change Nick"] ):
+	#Hide part and quit messages after timeout
+	if ( last_seen[user_ip][0] + user_timeout < time() and event not in ["Join", "Change Nick"] ):
 		if debug_output:
-			print("\00315Supressed old user event " + event + " from user " + user)
+			print("\00315Supressed old user event " + event + " from user " + user_name)
 		return hexchat.EAT_ALL
 
 def get_ip(host):
 	try:
 		ip = socket.gethostbyname(host.split('@')[1])
 	except Exception as e:
-		ip = host.split('@')[1]
+		ip = host.split('@')[1:]
 	return ip
 
-def get_geoip(host):
+def get_geoip(ip):
 	global geoip_output
 	global debug_output
 	geoip = ""
-	if geoip_output and (len(''.join(host.split('@')[1:]).split('.')) > 1 or len(''.join(host.split('@')[1:]).split(':'))):
-		ip = get_ip(host)
+	if geoip_output and (len(''.join(ip).split('.')) > 1 or len(''.join(ip).split(':') > 1)):
 		try:
 			if sys.version_info[0] < 3:
 				data = json.loads(urllib2.urlopen("http://ip-api.com/json/" + ip).read().decode("utf-8"))
